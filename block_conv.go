26e11d2a638a64409b12746a7d5abf613d997884
noahwill <noah.will.lawrence@gmail.com>
2022-01-15 12:45:34 -0600

    plain text input element

--- block_conv.go
+++ block_conv.go
@@ -60,6 +60,8 @@ func (b *Blocks) UnmarshalJSON(data []byte) error {
 			block = &ImageBlock{}
 		case "section":
 			block = &SectionBlock{}
+		case "input":
+			block = &InputBlock{}
 		default:
 			return errors.New("unsupported block type")
 		}
